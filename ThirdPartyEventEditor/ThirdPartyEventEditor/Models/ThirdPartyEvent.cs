589da394c0fa506c4a03a1a993d10d5bfff80d73
Ivan_Taturevich <ivan_taturevich@epam.com>
2021-05-20 15:31:41 +0300

    Added project for Task 3

---
+++ ThirdPartyEventEditor/ThirdPartyEventEditor/Models/ThirdPartyEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ThirdPartyEventEditor.Models
+{
+    public class ThirdPartyEvent
+    {
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Description { get; set; }
+
+        public string PosterImage { get; set; }
+    }
+}
