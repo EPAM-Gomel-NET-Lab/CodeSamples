313b46297b7de7ea5250c8791d972814278f2d52
Ivan_Taturevich <pollux094@gmail.com>
2021-08-12 16:20:58 +0200

    added missing files for TPL samples

---
+++ TPL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TPL
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var samples = new List<IAsyncCodeSample>
+            {
+                new NonAsyncSample(),
+                new Syncronization(),
+                new Cancellation(),
+                new DuckTypingAwaitable(),
+            };
+            foreach (var asyncCodeSample in samples)
+            {
+                Console.WriteLine(asyncCodeSample.SampleDescription);
+                await asyncCodeSample.Run();
+                Console.WriteLine("Press enter to continue");
+                Console.ReadLine();
+            }
+        }
+    }
+}
